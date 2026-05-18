66818505611c3cf58dd7ea72568cf849687c083e
Miriam <miriamsd94@gmail.com>
2026-05-18 11:19:57 +0200

    Exclude _id field from restaurant query results
    Updated query to exclude the _id field from results.

--- query/queries.js
+++ query/queries.js
@@ -1,5 +1,5 @@
 // 1. Mostrar tots els documents en la col·lecció Restaurants.
-db.restaurants.find()
+db.restaurants.find({}, { _id: 0 })
 
 // 2. Mostrar el restaurant_id i name per tots els documents en la col·lecció Restaurants.
 
