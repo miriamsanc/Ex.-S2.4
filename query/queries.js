f81336a1815f14af70fa9b0b7a571c13f59cabc2
Miriam <miriamsd94@gmail.com>
2026-05-18 19:36:32 +0200

    Update queries.js

--- query/queries.js
+++ query/queries.js
@@ -20,7 +20,7 @@ db.restaurants.find({ borough: "Bronx" },{_id: 0}).limit(5)
 db.restaurants.find({borough: "Bronx"},{_id: 0}).skip(5).limit(5)
 
 // 8. Trobar els restaurants amb un score de més de 90.
-
+db.restaurants.find({"grades.score":{ $gte: 90 } },{_id: 0})
 
 // 9. Trobar els restaurants amb un score de més de 80 però menys que 100.
 
